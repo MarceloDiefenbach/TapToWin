df761a63d9424df4cc663e8ecd4a6749beb5afe1
Marcelo Diefenbach <58398373+MarceloDiefenbach@users.noreply.github.com>
2022-11-05 09:50:43 -0300

    Initial Commit

---
+++ TapToWin/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TapToWin
+//
+//  Created by Marcelo Diefenbach on 05/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TapToWin/TapToWinApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapToWinApp.swift
+//  TapToWin
+//
+//  Created by Marcelo Diefenbach on 05/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TapToWinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
